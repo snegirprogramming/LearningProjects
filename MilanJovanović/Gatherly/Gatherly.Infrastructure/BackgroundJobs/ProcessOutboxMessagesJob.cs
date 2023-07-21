878b557d347214abacd56878283e2793045f2f05
Aleksandre Kapanadze <aleksandre.kapanadze@external.appspace.com>
2023-07-21 20:16:01 +0400

    How To Build Resilient Applications With Polly

--- MilanJovanović/Gatherly/Gatherly.Infrastructure/BackgroundJobs/ProcessOutboxMessagesJob.cs
+++ MilanJovanović/Gatherly/Gatherly.Infrastructure/BackgroundJobs/ProcessOutboxMessagesJob.cs
@@ -4,6 +4,8 @@ using Gatherly.Persistence.Outbox;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
 using Quartz;
 
 namespace Gatherly.Infrastructure.BackgroundJobs;
@@ -11,6 +13,11 @@ namespace Gatherly.Infrastructure.BackgroundJobs;
 [DisallowConcurrentExecution]
 public class ProcessOutboxMessagesJob : IJob
 {
+    private static readonly JsonSerializerSettings JsonSerializerSettings = new()
+    {
+        TypeNameHandling = TypeNameHandling.All
+    };
+
     private readonly ApplicationDbContext _dbContext;
     private readonly IPublisher _publisher;
 
@@ -24,22 +31,32 @@ public class ProcessOutboxMessagesJob : IJob
     {
         List<OutboxMessage> messages = await _dbContext
             .Set<OutboxMessage>()
-            .Where(m => m.ProcessedOnUtc == null)
+            .Where(m => m.ProcessedOnUtc == null &&
+                        m.Error == null)
             .Take(20)
-            .ToListAsync();
+            .ToListAsync(context.CancellationToken);
 
         foreach (OutboxMessage outboxMessage in messages)
         {
             IDomainEvent? domainEvent = JsonConvert
-                .DeserializeObject<IDomainEvent>(outboxMessage.Content);
+                .DeserializeObject<IDomainEvent>(outboxMessage.Content, JsonSerializerSettings);
 
             if (domainEvent is null)
             {
                 continue;
             }
 
-            await _publisher.Publish(domainEvent, context.CancellationToken);
+            AsyncRetryPolicy policy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(
+                    3,
+                    attempt => TimeSpan.FromMilliseconds(50 * attempt));
 
+            PolicyResult result = await policy
+                .ExecuteAndCaptureAsync(() => _publisher
+                    .Publish(domainEvent, context.CancellationToken));
+
+            outboxMessage.Error = result.FinalException?.ToString();
             outboxMessage.ProcessedOnUtc = DateTime.UtcNow;
         }
 
