a9b8c10e7374020f961710255955e7a1a3dcdddb
Aleksandre Kapanadze <aleksandre.kapanadze@external.appspace.com>
2023-08-06 21:23:32 +0400

    Prototype Design Pattern (C#)

---
+++ RawCoding/DesignPatterns/Prototype/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Prototype;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var garden = new GardenFactory(
+            new TreeSeed("Apple"),
+            new GrassSeed("Green"),
+            new FlowerSeed("Roses")
+        );
+
+        garden.CreatePlant1();
+        garden.CreatePlant2();
+        garden.CreatePlant3();
+    }
+}
+
+public class GardenFactory
+{
+    Seed _seed1;
+    Seed _seed2;
+    Seed _seed3;
+
+    public GardenFactory(Seed seed1, Seed seed2, Seed seed3)
+    {
+        _seed1 = seed1;
+        _seed2 = seed2;
+        _seed3 = seed3;
+    }
+
+    internal Seed CreatePlant1() => _seed1.Copy();
+    internal Seed CreatePlant2() => _seed2.Copy();
+    internal Seed CreatePlant3() => _seed3.Copy();
+}
+
+public abstract class Seed
+{
+    public abstract Seed Copy();
+}
+
+public class TreeSeed : Seed
+{
+    public string Type { get; }
+    public TreeSeed(string type) => Type = type;
+    public override Seed Copy() => new TreeSeed(Type);
+}
+
+public class GrassSeed : Seed
+{
+    public string Type { get; }
+    public GrassSeed(string type) => Type = type;
+    public override Seed Copy() => new GrassSeed(Type);
+}
+
+public class FlowerSeed : Seed
+{
+    public string Type { get; }
+    public FlowerSeed(string type) => Type = type;
+    public override Seed Copy() => new FlowerSeed(Type);
+}
